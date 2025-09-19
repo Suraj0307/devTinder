3e5475a60689373ac06b71e0258faf989ff72511
Suraj Joshi <joshisuraj535@gmail.com>
2025-09-19 23:07:42 +0530

    Season 2 Episode 14

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const { userAuth } = require("../middlewares/auth");
 const userRouter = express.Router();
 const { ConnectionRequest } = require("../models/connectionRequest");
 const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl", "age", "gender", "about", "skills"];
+const User = require("../models/user");
 
 // getting all pending connection request for the loggedIn User
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -43,6 +44,41 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         res.status(200).json(data);
     } catch (error) {
         console.log("Error fetching user connections:", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+// Getting the feed for the logged-in user
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+
+        limit = limit > 50 ? 50 : limit; // max limit is 50
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id},
+                { toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId");
+        
+        const hideUsersFromFeed = new Set();
+        hideUsersFromFeed.add(loggedInUser._id.toString());
+
+        connectionRequests.forEach((request) => {
+            hideUsersFromFeed.add(request.fromUserId.toString());
+            hideUsersFromFeed.add(request.toUserId.toString());
+        });
+
+        const users = await User.find({
+            _id: { $nin: Array.from(hideUsersFromFeed) }
+        }, USER_SAFE_DATA).skip((page - 1) * limit).limit(limit);
+
+        res.status(200).json(users);
+    } catch (error) {
+        console.log("Error fetching users for feed:", error.message);
         res.status(500).json({ error: "Internal server error" });
     }
 });
